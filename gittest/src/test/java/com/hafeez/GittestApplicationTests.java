2cecee4ae71130e4ea8f6f0fb7239af64e183a0c
hafeez8165526 <Hp@PC-ARCH>
2023-03-19 19:22:19 +0530

    first commit

---
+++ gittest/src/test/java/com/hafeez/GittestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hafeez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
